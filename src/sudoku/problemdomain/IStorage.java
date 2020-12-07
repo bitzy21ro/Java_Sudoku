58874a3c7f57aa0cc255f7d495848e760a189082
Bitzy <tudorache.alexandru08@gmail.com>
2020-12-07 06:34:35 +0200

    Inital commit

---
+++ src/sudoku/problemdomain/IStorage.java
@@ -0,0 +1,8 @@
+package sudoku.problemdomain;
+
+import java.io.IOException;
+
+public class IStorage {
+   void updateGameData(SudokuGame) throws IOException;
+   SudokuGame getGameData() throws IOException;
+}
